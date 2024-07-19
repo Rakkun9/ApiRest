20fb77385fae15640f0ebfb8b48cd0ea864ca70a
Rakun <jnicolaybv@gmail.com>
2024-07-19 00:02:58 -0500

    Add User model and serializers for API endpoints

--- mysite/api/models.py
+++ mysite/api/models.py
@@ -9,3 +9,14 @@ class BlogPost(models.Model):
 
     def __str__(self): 
         return self.title
+    
+class User(models.Model):
+    first  = models.CharField(max_length=100)
+    email = models.EmailField()
+    gender = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self): 
+        return self.first
